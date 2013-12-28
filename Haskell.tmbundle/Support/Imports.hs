02ec00c91c960719195d05d05a4fc1222689a6e9
Heinrich Apfelmus <apfelmus@quantentunnel.de>
2013-12-28 12:46:11 +0100

    Separate 'Imports' module. Parse 'hiding' clause.

---
+++ Haskell.tmbundle/Support/Imports.hs
@@ -0,0 +1,79 @@
+{-----------------------------------------------------------------------------
+    textmate-bundle
+------------------------------------------------------------------------------}
+{-# LANGUAGE RecordWildCards #-}
+module Imports where
+
+import Control.Applicative
+import Data.Char
+import Data.List
+import Data.Ord
+import Text.ParserCombinators.ReadP as P
+
+type Parser = P.ReadP
+
+{-----------------------------------------------------------------------------
+    Import manipulations
+------------------------------------------------------------------------------}
+tidyImports :: String -> String
+tidyImports = prettyImports . parseImports
+
+data Import = Import
+    { iQual    :: Bool
+    , iModule  :: String
+    , iAlias   :: Maybe String
+    , iHiding  :: Bool
+    , iSymbols :: [String]
+    } deriving (Eq, Ord, Show, Read)
+
+-- pretty printing
+prettyImports imports = unlines . map format . sortOn iModule $ imports
+    where
+    sortOn f = sortBy (comparing f)
+    
+    format (Import{..}) =
+        "import"
+        ++ (if iQual then " qualified " else qualspaces)
+        ++ iModule ++ padding maxmodule (length iModule)
+        ++ maybe "" (" as " ++) iAlias ++ padding maxalias (lengthAlias iAlias)
+        ++ prettySymbols (iHiding, iSymbols)
+    
+    padding max n = replicate (max - n) ' '
+    lengthAlias   = maybe 0 length
+    maxalias      = maximum $ map ((length " as " +) . lengthAlias . iAlias) imports 
+    maxmodule     = maximum $ map (length . iModule) imports
+    qualspaces    = if any iQual imports then map (const ' ') " qualified " else " "
+
+prettySymbols (hiding, []) = ""
+prettySymbols (hiding, xs) =
+    (if hiding then " hiding" else "")
+    ++ " (" ++ concat (intersperse ", " xs) ++ ")"
+
+
+-- parsing
+parseImports  = fst . last . readP_to_S pImports
+
+-- TODO: use some sort of a lexer
+pImports :: Parser [Import]
+pImports = skipSpaces *> manyTill pImport ((() <$ satisfy ('i' /=)) <++ eof)
+
+pImport = Import <$ string "import" <* skipSpaces
+    <*> pQual    <* skipSpaces
+    <*> pModule  <* skipSpaces
+    <*> pAlias   <* skipSpaces
+    <*> pHiding  <* skipSpaces
+    <*> pSymbols <* skipSpaces
+
+pQual    = option False (True <$ string "qualified")
+pHiding  = option False (True <$ string "hiding"   )
+pModule  = munch1 $ \c -> c == '.' || isAlpha c
+pAlias   = leftoption Nothing (Just <$> (string "as" *> skipSpaces *> pModule))
+-- FIXME: Constructor field T(..) are not parsed correctly.
+pSymbols = leftoption [] $ string "(" *> skipSpaces *>
+    sepBy pSymbol (skipSpaces *> string "," *> skipSpaces)
+    <* skipSpaces <* string ")"
+pSymbol  = munch1 $ \c -> isAlpha c || c == '\''
+
+leftoption a p = p <++ return a
+
+
--- Haskell.tmbundle/Support/Utils.hs
+++ Haskell.tmbundle/Support/Utils.hs
@@ -3,17 +3,10 @@
     
     Shared utility functions
 ------------------------------------------------------------------------------}
-{-# LANGUAGE RecordWildCards #-}
 module Utils where
 
-import Control.Applicative
-import Data.Char
 import Data.List
-import Data.Ord
 import System.FilePath
-import Text.ParserCombinators.ReadP as P
-
-type Parser = P.ReadP
 
 {-----------------------------------------------------------------------------
     file path manipulations
@@ -26,61 +19,6 @@ parentDirs = map joinPath . tail . inits . init . splitPath
 -- | Generate command line arguments to include parent directories
 includes :: FilePath -> [String]
 includes = map ("-i"++) . parentDirs
-
-
-{-----------------------------------------------------------------------------
-    import manipulations
-------------------------------------------------------------------------------}
-tidyImports :: String -> String
-tidyImports = prettyImports . parseImports
-
-data Import = Import
-    { iQual    :: Bool
-    , iModule  :: String
-    , iAlias   :: Maybe String
-    , iSymbols :: [String]
-    } deriving (Eq, Ord, Show, Read)
-
--- pretty printing
-prettyImports imports = unlines . map format . sortOn iModule $ imports
-    where
-    sortOn f = sortBy (comparing f)
-    
-    format (Import{..}) =
-        "import"
-        ++ (if iQual then " qualified " else qualspaces)
-        ++ iModule ++ padding (length iModule)
-        ++ maybe "" (" as " ++) iAlias
-        ++ prettySymbols iSymbols
-    
-    padding n  = replicate (n - maxmodule) ' '
-    maxmodule  = maximum $ map (length . iModule) imports
-    qualspaces = if any iQual imports then map (const ' ') " qualified " else " "
-
-prettySymbols [] = ""
-prettySymbols xs = " (" ++ concat (intersperse ", " xs) ++ " )"
-
--- parsing
-parseImports  = fst . last . readP_to_S pImports
-
-pImports :: Parser [Import]
-pImports = skipSpaces *> manyTill pImport ((() <$ satisfy ('i' /=)) <++ eof)
-
-pImport = Import <$ string "import" <* skipSpaces
-    <*> pQual    <* skipSpaces
-    <*> pModule  <* skipSpaces
-    <*> pAlias   <* skipSpaces
-    <*> pSymbols <* skipSpaces
-
-pQual    = option False (True <$ string "qualified") <* skipSpaces
-pModule  = munch1 $ \c -> c == '.' || isAlpha c
-pAlias   = leftoption Nothing (Just <$> (string "as" *> skipSpaces *> pModule))
-pSymbols = leftoption [] $ string "(" *>    
-    sepBy pSymbol (skipSpaces *> string "," *> skipSpaces)
-    <* string ")"
-pSymbol  = munch1 $ \c -> isAlpha c || c == '\''
-
-leftoption a p = p <++ return a
 
 
 
